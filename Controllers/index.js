0b93f10db13021ba9a1554993a2d785efd540956
lokeshfsg <sarakanamlokesh15@gmail.com>
2025-10-07 12:17:50 +0530

    Update index.js

--- Controllers/index.js
+++ Controllers/index.js
@@ -163,7 +163,7 @@ const login = async (req, res) => {
 
 // Lucky
 const lucky = (req, res) => {
-  console.log("User already exists:", email);
+  
   return res.status(200).json({ message: "User already exists" });
 };
 
